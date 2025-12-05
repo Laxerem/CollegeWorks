309f68ae366bc91c34406546e1efad0770c520c9
Laxerem <grishaMas@yandex.ru>
2025-12-05 15:30:30 +0500

    [UPDATE]

---
+++ aisd-3/src/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿var inputPath = "data.txt";
+var outputPath = "result.txt";
+
+var inputString = File.ReadAllText(inputPath);
+var valueArray = inputString.Split(' ').Select(int.Parse).ToArray();
+
+File.WriteAllText(outputPath, "");
+
+for (int i = 0; i < valueArray.Length; i++) {
+    if (valueArray[i] > 10) {
+        File.AppendAllText(outputPath, valueArray[i] + " ");
+    }
+}
+
+Console.WriteLine("Программа выполнена");
